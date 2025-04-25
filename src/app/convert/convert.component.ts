048b4f0cfa6025d31faf902f5ef00603b11e3c90
Hanna Angeria <haan2402@student.miun.se>
2025-04-25 14:21:27 +0200

    gjort komponent till att konvertera olika värden samt lagt in kod på convert html sida

--- src/app/convert/convert.component.ts
+++ src/app/convert/convert.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-convert',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './convert.component.html',
   styleUrl: './convert.component.css'
 })
 export class ConvertComponent {
 
+  //properties
+  value: number = 0;
+  result: number = 0;
+
+  //methods to convert
+  celsiusToFahrenheit(): void {
+    this.result = this.value * 9/5 + 32;
+  }
+
+ fahrenheitToCelsius(): void {
+  this.result = (this.value - 32) * (5/9);
+  }
+
+  meterToFeet(): void {
+    this.result = this.value * 3.2808;
+  }
+
+  feetToMeter(): void {
+    this.result = this.value / 3.2808;
+  }
 }
